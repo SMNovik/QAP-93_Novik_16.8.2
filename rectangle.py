44fa80d25fc013ac2180dffb055df0f00f296f16
Sergey Novik <110816135+SMNovik@users.noreply.github.com>
2022-09-30 13:21:50 +0300

    Add files via upload
    class constructor

---
+++ rectangle.py
@@ -0,0 +1,28 @@
+﻿class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+    def getWidth(self):
+        return self.width
+    
+    def getHeight(self):
+        return self.height
+    
+    def getArea(self):
+        return self.width * self.height
+
+class Square:
+    def __init__(self, side):
+        self.side = side
+
+    def getSquare(self):
+        return self.side**2
+
+class Circle:
+    def __init__(self, radius, pi=3.14):
+        self.radius = radius
+        self.pi = pi
+
+    def getAreaCircle(self):
+        return self.pi * self.radius**2
